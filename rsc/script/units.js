330d42c370799404db9faf1e6f8e3efdc80254fa
NuzoweR <nuzower@gmail.com>
2015-08-15 12:43:51 +0200

    add new player unit icons

--- rsc/script/units.js
+++ rsc/script/units.js
@@ -147,13 +147,13 @@ tso.data.live.functions.defineUnits = function () {
     pu.crossbowman   = new Unit("Crossbowman",    10, [45,  90],  80, Initiative.SECOND, getAID(), [Skills.TOWER_BONUS], "icon_military_crossbowman01.png");
     pu.cannoneer     = new Unit("Cannoneer",      60, [60, 120],  90, Initiative.THIRD,  getAID(), [Skills.TOWER_BONUS, Skills.ARMOR_PENETRATION, Skills.CAMP_DMG_BONUS], "icon_military_cannoneer01.png");
     
-    pu.swordsman     = new Unit("Swordsman",     150, [40,  80],  90, Initiative.SECOND, getAID(), [], ".png");
-    pu.mountedSMan   = new Unit("Mounted SMan",  250, [25,  50],  90, Initiative.SECOND, getAID(), [], ".png");
-    pu.knight        = new Unit("Knight",         80, [35,  70],  90, Initiative.FIRST,  getAID(), [], ".png");
-    pu.marksman      = new Unit("Marksman",       50, [50, 100],  90, Initiative.SECOND, getAID(), [], ".png");
-    pu.armoredMMan   = new Unit("Armored MMan",   50, [100,200],  90, Initiative.SECOND, getAID(), [], ".png");
-    pu.mountedMMan   = new Unit("Mounted MMan",   50, [50, 100],  90, Initiative.FIRST,  getAID(), [Skills.SPLASH_DAMAGE], ".png");
-    pu.besieger      = new Unit("Besieger",       80, [100,200],  90, Initiative.THIRD,  getAID(), [Skills.SPLASH_DAMAGE], ".png");
+    pu.swordsman     = new Unit("Swordsman",     150, [40,  80],  90, Initiative.SECOND, getAID(), [], "swordsman.png");
+    pu.mountedSMan   = new Unit("Mounted SMan",  250, [25,  50],  90, Initiative.SECOND, getAID(), [], "mounted_swordsman.png");
+    pu.knight        = new Unit("Knight",         80, [35,  70],  90, Initiative.FIRST,  getAID(), [], "knight.png");
+    pu.marksman      = new Unit("Marksman",       50, [50, 100],  90, Initiative.SECOND, getAID(), [], "marksman.png");
+    pu.armoredMMan   = new Unit("Armored MMan",   50, [100,200],  90, Initiative.SECOND, getAID(), [], "armored_marksman.png");
+    pu.mountedMMan   = new Unit("Mounted MMan",   50, [50, 100],  90, Initiative.FIRST,  getAID(), [Skills.SPLASH_DAMAGE], "mounted_marksman.png");
+    pu.besieger      = new Unit("Besieger",       80, [100,200],  90, Initiative.THIRD,  getAID(), [Skills.SPLASH_DAMAGE], "besieger.png");
 
     setUnitIDs(pu);
     setUnitClass(pu, EnemyType.PLAYER);
